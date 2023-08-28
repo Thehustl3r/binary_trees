1df16085258fbe72f6befb5454cbfcd30e51d1ae
mugishab2020 <mugishab2020@gmail.com>
2023-08-28 10:48:40 -0700

    Pre_order is done

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - function for preorder ttraversal
+ * @tree: the tree to traversal
+ * @func: the function to visit all nodes
+ * Return: nothing
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if ((tree == NULL) || (func == NULL))
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
